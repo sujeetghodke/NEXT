36ab9513419c7c033e73800065508ac665a9c125
sujeetghodke <sujeetghodke16@gmail.com>
2024-07-17 22:44:03 +0530

    music-academy project started

---
+++ music-academy/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex bg-black min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl text-white bg-gray-950">This is music academy</h1>
+    </main>
+  );
+}
